03e3cceda4c54a445d4c34af7c443a5f2f09b598
Nicolò Fretti <nico.fretti@gmail.com>
2020-09-18 18:28:40 +0200

    Player and tile

--- Components/Player/Player.gd
+++ Components/Player/Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
 
-const SPEED = 300
+const SPEED = 15000
+var angle = 11
+
 func _process(delta):
 	var direction = Vector2()
 	if Input.is_action_pressed("ui_down"):
@@ -12,25 +14,21 @@ func _process(delta):
 		direction.x += -1
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
-	var collision = move_and_collide(direction.normalized()*SPEED*delta)
+	if Input.is_action_just_pressed("ui_debug"):
+		pass
+	var collision = move_and_slide(direction.normalized()*SPEED*delta)
 	update_animation(direction)
-	
-	var mpos = get_global_mouse_pos()
-	look_at(mpos)
-	
 
+	$Sprite.rotation = get_local_mouse_position().angle()+angle
+	
 func update_animation(direction):
 	if(direction==Vector2()):
-		$Sprite.play("Idle")
-		$Sprite.flip_v=false
+		$Sprite.Idle()
 	elif direction.y==1:
-		$Sprite.play("Walk")
+		$Sprite.Walk()
 	elif direction.y==-1:
-		$Sprite.play("Walk")
-		$Sprite.flip_v=true
+		$Sprite.Walk()
 	elif direction.x==-1:
-		$Sprite.play("Walk")
-		$Sprite.flip_h=true
+		$Sprite.Walk()
 	elif direction.x==1:
-		$Sprite.play("Run")
-		$Sprite.flip_h=false
+		$Sprite.Walk()
